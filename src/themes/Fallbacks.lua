4dae0aadd8d125d60fc0919f28643f1598bcd855
d4p01 <dappearlsz@gmail.com>
2025-10-28 02:20:46 +0700

    Create Fallbacks.lua

---
+++ src/themes/Fallbacks.lua
@@ -0,0 +1,22 @@
+return {
+    Dialog = "Accent",
+    
+    Background = "Accent",
+    Hover = "Text",
+    
+    WindowBackground = "Background",
+    
+    TopbarButtonIcon = "Icon",
+    TopbarTitle = "Text",
+    TopbarAuthor = "Text",
+    TopbarIcon = "Text",
+    
+    TabBackground = "Hover",
+    TabTitle = "Text",
+    TabIcon = "Icon",
+    
+    ElementBackground = "Text",
+    ElementTitle = "Text",
+    ElementDesc = "Text",
+    ElementIcon = "Icon",
+}
